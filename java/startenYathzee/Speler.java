9b1f08d46f485104498e64bf80541849f4ee79cc
Ramonvdst <rawmon94@gmail.com>
2021-09-15 17:28:05 +0200

    update2

---
+++ java/startenYathzee/Speler.java
@@ -0,0 +1,14 @@
+package startenYathzee;
+
+import java.util.ArrayList;
+
+
+//Speler class, hiermee kan een speler worden aagemaakt
+//het bevat: 
+//		playerName = 	naam van de speler
+//		workHistory = 	de geschiedenis van elke dobbelsteenworp die de speler heeft uitgevoerd
+public class Speler {
+	String playerName;
+	ArrayList<String> worpHistory = new ArrayList<String>();
+	
+}
